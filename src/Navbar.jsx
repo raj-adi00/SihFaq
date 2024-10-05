55441bb749d858c1cc6d4e2765775cf2c61865d3
Chandrima Hazra <hazrachandrima6@gmail.com>
2024-10-05 16:03:36 +0530

    kuch nai

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './CSS/Navbar.css'
-import Logo from "/public/logo2.svg";
+import Logo from "../public/logo.svg";
 
 const Navbar = () => {
     return (
